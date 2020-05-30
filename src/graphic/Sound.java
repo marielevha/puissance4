bdce6882eda20f54b21dc89c725451533d8c3384
ssdlv <mspeedy733@gmail.com>
2020-05-31 00:28:10 +0200

    update

--- src/graphic/Sound.java
+++ src/graphic/Sound.java
@@ -5,25 +5,8 @@ import javazoom.jl.player.Player;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Sound {
-    public static void main(String[] args) {
-        ArrayList<String> mylist = new ArrayList<String>();
-        mylist.add("code");
-        mylist.add("quiz");
-        mylist.add("geeksforgeeks");
-        mylist.add("quiz");
-        mylist.add("practice");
-        mylist.add("qa");
-
-        System.out.println("Original List : \n" + mylist);
-
-        Collections.shuffle(mylist);
-
-        System.out.println("\nShuffled List : \n" + mylist);
-    }
     public static void playMusic() {
         int count = 0;
         while (true) {
